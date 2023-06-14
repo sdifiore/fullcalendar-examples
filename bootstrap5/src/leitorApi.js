37a91d5280b5f58c7e197d66a720aaa3e2438af8
Sergio Di Fiore <difiores@outlook.com>
2023-06-14 15:43:15 -0300

    Antes substituição Index.js Co-authored-by: Davi Luna <Davijluna@users.noreply.github.com>

--- bootstrap5/src/leitorApi.js
+++ bootstrap5/src/leitorApi.js
@@ -1,15 +1,292 @@
-// const axios = require('axios');
+axios('http://difiores-001-site3.etempurl.com/Agenda')
+.then(resposta => carregaElementoNaPagina(resposta.data))
 
-// const url = "http://difiores-001-site3.etempurl.com/Agenda";
+ const carregaElementoNaPagina = (json) => {
+  console.log(json)
 
-// axios.get(url)
-//   .then(response => {
-//     const json = response.data;
-//     console.log(json);
+  const table = document.createElement('table');
 
-//     const jsonString = JSON.stringify(json);
-//     console.log(jsonString);
-//   })
-//   .catch(error => {
-//     console.error('Erro na solicitação HTTP:', error);
-//   });
+import {​​​​​​ Calendar }​​​​​​ from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // webpack uses file-loader to handle font files
+import './index.css'; // our app's CSS
+
+document.addEventListener('DOMContentLoaded', function() {​​​​​​
+ var calendarEl = document.getElementById('calendar');
+ var calendar = new Calendar(calendarEl, {​​​​​​
+ locale: 'pt-br',
+ plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, bootstrap5Plugin ],
+ themeSystem: 'bootstrap5',
+ headerToolbar: {​​​​​​
+ left: 'prev,next today',
+ center: 'title',
+ right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+ }​​​​​​,
+ initialDate: '2023-06-12',
+ navLinks: true, // can click day/week names to navigate views
+ editable: true,
+ dayMaxEvents: true, // allow "more" link when too many events
+ events: function(inf, successCallback, failureCallback) {​​​​​​
+ var url = 'http://difiores-001-site3.etempurl.com/Agenda'
+ fetch(url)
+ .then(response => {​​​​​​
+ if(response) {​​​​​​
+ return response.json();
+ }​​​​​​ else {​​​​​​
+ throw new Error('Erro na solicitação GET. ' + response.status);
+ }​​​​​​
+ }​​​​​​)
+ .then(data => {​​​​​​
+ successCallback(data);
+ }​​​​​​)
+ .catch(error => {​​​​​​
+ console.error('Ocorreu um erro:', error);
+ failureCallback(error)
+ }​​​​​​)
+ }​​​​​​
+ // 'http://difiores-001-site3.etempurl.com/Agenda'
+ // [
+ // {​​​​​​
+ // agendaId: 1,
+ // aluno: "João",
+ // professor: "Silva",
+ // disciplina: "Matemática",
+ // data: "01/06/2023",
+ // horaInicio: "09:00",
+ // horaTermino: "15:00",
+ // local: "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // agendaId: 2,
+ // aluno: "Maria",
+ // professor: "Santos",
+ // disciplina: "Ciências",
+ // data: "02/06/2023",
+ // horaInicio: "10:00",
+ // horaTermino: "16:00",
+ // local: "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // agendaId: 3,
+ // aluno: "Lucas",
+ // professor: "Pereira",
+ // disciplina: "História",
+ // data: "03/06/2023",
+ // horaInicio: "11:00",
+ // horaTermino: "17:00",
+ // local: "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 4,
+ // "aluno": "Ana Paula",
+ // "professor": "Costa",
+ // "disciplina": "Geografia",
+ // "data": "09/06/2023",
+ // "horaInicio": "12:00",
+ // "horaTermino": "18:00",
+ // "local": "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 5,
+ // "aluno": "Paulo",
+ // "professor": "Souza",
+ // "disciplina": "Português",
+ // "data": "12/06/2023",
+ // "horaInicio": "13:00",
+ // "horaTermino": "19:00",
+ // "local": "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 6,
+ // "aluno": "Manuela",
+ // "professor": "Oliveira",
+ // "disciplina": "Inglês",
+ // "data": "15/06/2023",
+ // "horaInicio": "14:00",
+ // "horaTermino": "20:00",
+ // "local": "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 7,
+ // "aluno": "Pedro",
+ // "professor": "Lima",
+ // "disciplina": "Artes",
+ // "data": "18/06/2023",
+ // "horaInicio": "09:00",
+ // "horaTermino": "15:00",
+ // "local": "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 8,
+ // "aluno": "Julia",
+ // "professor": "Moraes",
+ // "disciplina": "Física",
+ // "data": "19/06/2023",
+ // "horaInicio": "10:00",
+ // "horaTermino": "16:00",
+ // "local": "Sala 1"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 9,
+ // "aluno": "Carlos",
+ // "professor": "Ribeiro",
+ // "disciplina": "Física",
+ // "data": "01/06/2023",
+ // "horaInicio": "09:00",
+ // "horaTermino": "15:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 10,
+ // "aluno": "Fernanda",
+ // "professor": "Rocha",
+ // "disciplina": "Química",
+ // "data": "07/06/2023",
+ // "horaInicio": "16:00",
+ // "horaTermino": "10:00",
+ // "local": "16:00"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 11,
+ // "aluno": "Gabriel",
+ // "professor": "Almeida",
+ // "disciplina": "Biologia",
+ // "data": "08/06/2023",
+ // "horaInicio": "11:00",
+ // "horaTermino": "17:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 12,
+ // "aluno": "Bianca",
+ // "professor": "Gomes",
+ // "disciplina": "Literatura",
+ // "data": "09/06/2023",
+ // "horaInicio": "12:00",
+ // "horaTermino": "18:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 13,
+ // "aluno": "Ricardo",
+ // "professor": "Barros",
+ // "disciplina": "Geometria",
+ // "data": "10/06/2023",
+ // "horaInicio": "13:00",
+ // "horaTermino": "19:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 14,
+ // "aluno": "Teresa",
+ // "professor": "Castro",
+ // "disciplina": "Algebra",
+ // "data": "11/06/2023",
+ // "horaInicio": "14:00",
+ // "horaTermino": "20:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 15,
+ // "aluno": "Renato",
+ // "professor": "Cardoso",
+ // "disciplina": "Música",
+ // "data": "12/06/2023",
+ // "horaInicio": "09:00",
+ // "horaTermino": "15:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 16,
+ // "aluno": "Daniela",
+ // "professor": "Nunes",
+ // "disciplina": "Filosofia",
+ // "data": "01/06/2023",
+ // "horaInicio": "10:00",
+ // "horaTermino": "12:00",
+ // "local": "Sala 2"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 17,
+ // "aluno": "Guilherme",
+ // "professor": "Teixeira",
+ // "disciplina": "Sociologia",
+ // "data": "06/06/2023",
+ // "horaInicio": "09:00",
+ // "horaTermino": "15:00",
+ // "local": "Sala 3"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 18,
+ // "aluno": "Clarissa",
+ // "professor": "Moreira",
+ // "disciplina": "Psicologia",
+ // "data": "07/06/2023",
+ // "horaInicio": "10:00",
+ // "horaTermino": "16:00",
+ // "local": "Sala 3"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 19,
+ // "aluno": "André",
+ // "professor": "Mendonça",
+ // "disciplina": "Economia",
+ // "data": "08/06/2023",
+ // "horaInicio": "11:00",
+ // "horaTermino": "17:00",
+ // "local": "Sala 3"
+ // }​​​​​​,
+ // {​​​​​​
+ // "agendaId": 20,
+ // "aluno": "Aline",
+ // "professor": "Barreto",
+ // "disciplina": "Política",
+ // "data": "06/06/2023",
+ // "horaInicio": "11:00",
+ // "horaTermino": "14:00",
+ // "local": "Sala 3"
+ // }​​​​​​
+ // ]
+ // var jsonString = JSON.stringify(objetoJSON);
+ // console.log(jsonString);
+ // console.log(jsonString[5]);
+ // ]
+ }​​​​​​);
+ calendar.render();
+}​​​​​​);
+
+    const tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerHTML = pessoa.aluno;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.professor;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.disciplina;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.data;
+    tr.appendChild(td);
+
+    // Aqui inserimos a tr como filha de table
+
+    table.appendChild(tr)
+
+    const resultado = document.querySelector('.resultado')
+
+    resultado.appendChild(table)
+
+    console.log(pessoa.nome)
+  }
+ }
+
